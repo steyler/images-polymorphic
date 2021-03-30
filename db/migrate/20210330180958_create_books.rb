800366ec739ee71a4d71187aeff08d68e8232c45
Steyler <steylerap@gmail.com>
2021-03-30 12:12:58 -0600

    add book scaffold

---
+++ db/migrate/20210330180958_create_books.rb
@@ -0,0 +1,10 @@
+class CreateBooks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :books do |t|
+      t.string :author
+      t.string :title, null: false
+
+      t.timestamps
+    end
+  end
+end
